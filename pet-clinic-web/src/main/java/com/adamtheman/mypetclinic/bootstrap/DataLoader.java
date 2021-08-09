e72015ce559eb6c0cf3adf31654f755c46fbe0d9
“adamtheman” <adipostaja@gmail.com>
2021-08-09 08:51:54 +0200

    Added Vets List and Updated Controller. Closes #15

--- pet-clinic-web/src/main/java/com/adamtheman/mypetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/adamtheman/mypetclinic/bootstrap/DataLoader.java
@@ -38,7 +38,7 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(2L);
+        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastname("Axe");
 
